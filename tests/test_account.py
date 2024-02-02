a33db5b3f42db53d3495c0411784d0fb87e8bfa4
Intensifiesx <hindizain12@yahoo.com>
2024-02-02 15:31:10 -0800

    Getting 100% test coverage

--- tests/test_account.py
+++ tests/test_account.py
@@ -24,6 +24,33 @@ class TestAccountModel(TestCase):
     def tearDownClass(cls):
         """Disconnext from database"""
         db.session.close()
+        
+    def test_from_dict(self):
+        """ Test account from dictionary """
+        data = ACCOUNT_DATA[self.rand]
+        account = Account()
+        account.from_dict(data)
+        self.assertEqual(account.name, data["name"])
+        self.assertEqual(account.email, data["email"])
+        self.assertEqual(account.phone_number, data["phone_number"])
+        self.assertEqual(account.disabled, data["disabled"])
+        
+    def test_to_dict(self):
+        """ Test account to dict """
+        data = ACCOUNT_DATA[self.rand] # get a random account
+        account = Account(**data)
+        result = account.to_dict()
+        self.assertEqual(account.name, result["name"])
+        self.assertEqual(account.email, result["email"])
+        self.assertEqual(account.phone_number, result["phone_number"])
+        self.assertEqual(account.disabled, result["disabled"])
+        self.assertEqual(account.date_joined, result["date_joined"])
+            
+    def test_repr(self):
+        """Test the representation of an account"""
+        account = Account()
+        account.name = "Foo"
+        self.assertEqual(str(account), "<Account 'Foo'>")
 
     def setUp(self):
         """Truncate the tables"""
